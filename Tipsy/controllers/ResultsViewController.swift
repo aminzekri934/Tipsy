b28d8ac303be42cd5435a5b1c822f7f0ac7b51ae
aminzekri934 <aminzekri934@gmail.com>
2024-03-21 08:38:01 +0100

    first commit

--- Tipsy/controllers/ResultsViewController.swift
+++ Tipsy/controllers/ResultsViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
